1d70377106315896fc3d25bab5634aa3a5a1b9d9
IvanRistovic <ivo.ristovic@gmail.com>
2016-12-19 17:59:10 +0100

    Created Field class and refactored the code

---
+++ langton-ant/Field.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace langton_ant
+{
+    class Field
+    {
+        private const int CELL_SIZE = 15;
+
+        private Label[,] map;
+
+
+        public Field(Form form, int size)
+        {
+            map = new Label[size, size];
+
+            /* Starting location */
+            int horizotal = CELL_SIZE;
+            int vertical = CELL_SIZE;
+
+            /* Drawing field */
+            for (int i = 0; i < size; i++) {
+
+                for (int j = 0; j < size; j++) {
+
+                    /* Creating new label with given size */
+                    map[i, j] = new Label();
+                    map[i, j].Size = new Size(CELL_SIZE, CELL_SIZE);
+                    map[i, j].Location = new Point(horizotal, vertical);
+                    map[i, j].BorderStyle = BorderStyle.FixedSingle;
+
+                    horizotal += CELL_SIZE;
+
+                    /* Adding created label to control */
+                    form.Controls.Add(map[i, j]);
+                }
+
+                horizotal = CELL_SIZE;
+                vertical += CELL_SIZE;
+            }
+        }
+    }
+}
